446c82b0eb9414dd8ae7745f1bc7291dfb881e1c
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-06-27 07:56:27 +0500

    modified migration db file for new changes

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,16 +1,17 @@
 -- Users
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(100) NOT NULL UNIQUE,
+                       id BIGSERIAL PRIMARY KEY,
+                       email VARCHAR(100) NOT NULL UNIQUE,
                        password VARCHAR(255) NOT NULL,
                        full_name VARCHAR(100),
-                       created_at TIMESTAMP DEFAULT now()
+                       created_at TIMESTAMP DEFAULT now(),
+                       refresh_token VARCHAR(512)
 );
 
 -- Accounts
 CREATE TABLE account (
-                         id SERIAL PRIMARY KEY,
-                         user_id INTEGER NOT NULL ,
+                         id BIGSERIAL PRIMARY KEY,
+                         user_id BIGINT NOT NULL ,
                          name VARCHAR(100) NOT NULL,
                          currency VARCHAR(3) DEFAULT 'KZT',
                          balance NUMERIC(14, 2) DEFAULT 0,
@@ -22,8 +23,8 @@ CREATE TABLE account (
 
 -- Categories
 CREATE TABLE category (
-                          id SERIAL PRIMARY KEY,
-                          user_id INTEGER NOT NULL ,
+                          id BIGSERIAL PRIMARY KEY,
+                          user_id BIGINT NOT NULL ,
                           name VARCHAR(100) NOT NULL,
                           type VARCHAR(20) NOT NULL CHECK (type IN ('INCOME', 'EXPENSE')) ,
                           CONSTRAINT fk_user_id
@@ -33,8 +34,8 @@ CREATE TABLE category (
 
 -- Transactions
 CREATE TABLE transaction (
-                             id SERIAL PRIMARY KEY,
-                             user_id INTEGER NOT NULL ,
+                             id BIGSERIAL PRIMARY KEY,
+                             user_id BIGINT NOT NULL ,
                              account_id INTEGER NOT NULL ,
                              amount NUMERIC(14, 2) NOT NULL,
                              type VARCHAR(20) NOT NULL CHECK (type IN ('INCOME', 'EXPENSE', 'TRANSFER')),
@@ -54,7 +55,7 @@ CREATE TABLE transaction (
 
 -- Transfers (связаны с типом 'TRANSFER' в transaction)
 CREATE TABLE transfer (
-                          id SERIAL PRIMARY KEY,
+                          id BIGSERIAL PRIMARY KEY,
                           transaction_id INTEGER NOT NULL UNIQUE REFERENCES transaction(id) ON DELETE CASCADE,
                           from_account_id INTEGER NOT NULL REFERENCES account(id),
                           to_account_id INTEGER NOT NULL REFERENCES account(id),
