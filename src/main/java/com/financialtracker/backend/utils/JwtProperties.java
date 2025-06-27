7868fd58c4cd33f29e5d1b5a4cf46d811152c314
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-06-27 08:04:50 +0500

    created jwt properties for jwt usage

---
+++ src/main/java/com/financialtracker/backend/utils/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.financialtracker.backend.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+
+    private String secret;
+    private long accessTokenExpirationMs;
+    private long refreshTokenExpirationMs;
+}
