a7104ca8a2cfd2131e7363d872f2ff1bf792b091
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-07-16 07:34:12 +0500

    created account repository for account entity

--- src/main/java/com/financialtracker/backend/repository/AccountRepository.java
+++ src/main/java/com/financialtracker/backend/repository/AccountRepository.java
@@ -1,4 +1,11 @@
 package com.financialtracker.backend.repository;
 
-public interface AccountRepository {
+import com.financialtracker.backend.entity.Account;
+import com.financialtracker.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUser(User user);
 }
