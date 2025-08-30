fd105b1d09d70501cceb26dd76876f884ca86d3e
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-08-30 14:09:00 +0500

    created repository for the transaction entity

--- src/main/java/com/financialtracker/backend/repository/TransactionRepository.java
+++ src/main/java/com/financialtracker/backend/repository/TransactionRepository.java
@@ -1,4 +1,14 @@
 package com.financialtracker.backend.repository;
 
-public interface TransactionRepository {
+import com.financialtracker.backend.entity.Transaction;
+import com.financialtracker.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByAccountUser(User user);
 }
